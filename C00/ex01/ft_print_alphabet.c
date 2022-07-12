7a8ca95e46fee4102a56fb642079ad9458929c41
Andrea Vannini <anvannin@c1r3p6.42firenze.it>
2022-07-12 22:56:50 +0200

    Second

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anvannin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 21:21:34 by anvannin          #+#    #+#             */
+/*   Updated: 2022/07/12 22:40:35 by anvannin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	ft_putchar('a');
+	ft_putchar('b');
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
